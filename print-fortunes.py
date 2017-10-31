e5235692bdff0a70330ad952e068075e4523e3c0
rdeese <rdeese@hmc.edu>
2017-10-31 11:24:30 -0500

    update fortunes script.

--- print-fortunes.py
+++ print-fortunes.py
@@ -1,15 +1,16 @@
 import markovify
 import os
 
-FORTUNE_TEMPLATE = "\\fortune{{}}\n"
+FORTUNE_TEMPLATE = "\\fortune{FORTUNE}"
 
-with open('inputs/clean_corpus.py') as f:
+with open('inputs/quotes-daddy-life.txt') as f:
     corpus = f.read()
 
 model = markovify.Text(corpus, state_size=3)
-fortunes = [FORTUNE_TEMPLATE.format(model.make_short_sentence(60)) for _ in range(40)]
+fortunes = '\n'.join([FORTUNE_TEMPLATE.replace("FORTUNE", model.make_short_sentence(60)) for _ in range(40)])
+print(fortunes)
 
-with open('tex/fortunes.tex') as f:
+with open('tex/fortunes-template.tex') as f:
     template = f.read()
 
 with open('tex/fortunes.tex', 'w') as f:

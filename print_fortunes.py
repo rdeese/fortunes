81fad7fd399ec6836e8dd6fdbfd6b98f22d230d7
rdeese <rdeese@hmc.edu>
2017-11-06 12:06:28 -0600

    Generalize blacklist to cover more words.

--- print_fortunes.py
+++ print_fortunes.py
@@ -9,17 +9,20 @@ import markovify
 FORTUNE_TEMPLATE = "\\fortune{FORTUNE}{NUMBERS}"
 PICKLED_MODEL = 'fortune_model.pickle'
 
-FIRST_PERSON_PRONOUN = re.compile(r'(\W|\A)I(\W|\Z)|(\W|\A)me(\W|\Z)', flags=re.IGNORECASE)
+BLACKLIST_WORDS = ['I', 'me', 'my', 'him', 'his', 'he', 'her', 'hers', 'she']
+BLACKLIST_MATCH_TEMPLATE = r'(\W|\A){}(\W|\Z)'
+BLACKLIST_REGEX = '|'.join([BLACKLIST_MATCH_TEMPLATE.format(word) for word in BLACKLIST_WORDS])
+FORTUNE_BLACKLIST = re.compile(BLACKLIST_REGEX, flags=re.IGNORECASE)
 
 def lucky_numbers():
     """ Return a string of 6 ordered, comma-separated numbers """
-    sorted_numbers = sorted([random.randint(1, 100) for _ in range(6)])
+    sorted_numbers = sorted([random.randint(1, 100) for _ in range(12)])[:6]
     return ', '.join([str(x) for x in sorted_numbers])
 
 def valid_fortune(fortune):
     """ Whether this fortune is valid """
     return (fortune is not None and
-            FIRST_PERSON_PRONOUN.search(fortune) is None)
+            FORTUNE_BLACKLIST.search(fortune) is None)
 
 def main():
     """ Generate fortunes in a PDF """
